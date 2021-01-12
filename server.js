16a2b34feafc9a26219a5d0e4c7f38ee6dc4372e
Bmercado <askotala@yahoo.com>
2021-01-11 22:26:36 -0800

    finally

--- server.js
+++ server.js
@@ -1,47 +1,59 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+//////static files//////
+app.use(express.static("css"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/sass", express.static(__dirname + "sass/main.css"));
+app.use("/js", express.static(__dirname + "/server.js"));
+app.use("/img", express.static(__dirname + "/img"));
+// app.use("/css", express.static(__dirname + "sass/main.css"));
 
-//static files
-app.use(express.static("sass"))
-app.use("/main.scss", express.static(__dirname + "sass/main.scss"))
+///
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-
-mongoose.connect("mongodb+srv://askotala:XXXXXXX@cluster0.0xkzj.mongodb.net/TodoApp", 
-   { useNewUrlParser: true,
-   useUnifiedTopology: true,
-   useCreateIndex: true,
- });
-
+mongoose.connect(
+  "mongodb+srv://askotala:@@@@@@&&@cluster0.0xkzj.mongodb.net/TodoApp",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
 
 //create data schema
- const notesSchema = {
-          title: String,
-          content: String 
-
- }
- const Note = mongoose.model("Note", notesSchema);
+const notesSchema = {
+  title: String,
+  content: String,
+};
+const Note = mongoose.model("Note", notesSchema);
 
 // app.use(express.static("sass"))
 // app.use("/sass",express.static(__dirname + "sass/css"))
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html")
-    });
-app.post("/", function(req,res){
-    let newNote = new Note ({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newNote.save()
-    res.redirect("/");
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+/////////////////////about us page/////////
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + "/views/aboutUs.html");
+// });
+app.post("/", function (req, res) {
+  let newNote = new Note({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newNote.save();
+  res.redirect("/");
+});
 
 //listen on port 3000
 
-app.listen(3000)
+app.listen(3000);
